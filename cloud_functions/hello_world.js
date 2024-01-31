402dee5d583888ac8298102f98ba773a11ecfccc
BCM <dahifi@gmail.com>
2024-01-31 09:59:30 -0500

    feat: successful cors hello world

---
+++ cloud_functions/hello_world.js
@@ -0,0 +1,25 @@
+const functions = require('@google-cloud/functions-framework');
+
+/**
+ * HTTP function that supports CORS requests.
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+functions.http('helloWorld', (req, res) => {
+  // Set CORS headers for preflight requests
+  // Allows GETs from any origin with the Content-Type header
+  // and caches preflight response for 3600s
+
+  res.set('Access-Control-Allow-Origin', '*');
+
+  if (req.method === 'OPTIONS') {
+    // Send response to OPTIONS requests
+    res.set('Access-Control-Allow-Methods', 'GET');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');
+  } else {
+    res.send('Hello World!');
+  }
+});
