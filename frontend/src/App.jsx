ddea7f0d15a428800581266822cdab711880ea74
BCM <dahifi@gmail.com>
2024-01-31 14:52:51 -0500

    fix: audio chunks handle correctly.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
 // src/App.jsx
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 import AudioRecorder from './components/AudioRecorder';
 import AudioPlayer from "./components/AudioPlayer.jsx";
 import HelloWorld from "./components/HelloWorld.jsx";
+import AudioUploader from "./components/AudioUploader.jsx";
 
 function App() {
       const [audioBlob, setAudioBlob] = useState(null);
 
     return (
         <div className="App">
+            <HelloWorld />
             <AudioRecorder setAudioBlob={setAudioBlob} />
             <AudioPlayer audioBlob={audioBlob} />
-            <HelloWorld />
+            <AudioUploader />
+
         </div>
     );
 }
--- frontend/src/components/AudioRecorder.jsx
+++ frontend/src/components/AudioRecorder.jsx
@@ -1,16 +1,22 @@
-import React, { useState } from 'react';
+// src/components/AudioRecorder.jsx
+import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const AudioRecorder = (props) => {
   const [isRecording, setIsRecording] = useState(false);
   const [mediaRecorder, setMediaRecorder] = useState(null);
-  const [audioChunks, setAudioChunks] = useState([]);
+  // const [audioChunks, setAudioChunks] = useState([]);
+  let audioChunks = [];
 
   const startRecording = async () => {
+    audioChunks = []
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      const recorder = new MediaRecorder(stream);
+      const recorder = new MediaRecorder(stream, {mimeType: "audio/webm"});
+
       recorder.ondataavailable = (event) => {
-        setAudioChunks((prevChunks) => [...prevChunks, event.data]);
+        console.info('Recorder data available', event);
+        audioChunks.push(event.data); // Collect chunks into the local array
       };
       recorder.onstart = () => {
         console.log('Recording started'); // Debugging log
@@ -19,15 +25,21 @@ const AudioRecorder = (props) => {
       recorder.onstop = () => {
         console.log('Recording stopped'); // Debugging log
         const audioBlob = new Blob(audioChunks, { type: 'audio/webm' });
+        console.log("Blob created", audioBlob)
         props.setAudioBlob(audioBlob);
         setIsRecording(false);
       };
+
       recorder.start();
       setMediaRecorder(recorder);
     } catch (error) {
       console.error('Error accessing the microphone', error);
     }
   };
+
+  AudioRecorder.propTypes = {
+  setAudioBlob: PropTypes.func.isRequired,
+};
 
   const stopRecording = () => {
     if (mediaRecorder) {
