1d3d75c28074eb7cd59cbcbfd9d8773a231bdc0b
BCM <dahifi@gmail.com>
2024-02-02 14:19:57 -0500

    feat: ASR returns transcript
    added bucket url base for vite config
    script to build and deploy static assets to google bucket
    ASRUploader component checks and output

--- frontend/src/components/ASRUploader.jsx
+++ frontend/src/components/ASRUploader.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
-class ASRUploader extends React.Component {
-  uploadAudio = async () => {
-    const { audioBlob, timestamp } = this.props;
+const ASRUploader = ({ audioBlob, timestamp }) => {
+  const [uploadStatus, setUploadStatus] = useState(''); // To keep track of the upload status
+  const [responseData, setResponseData] = useState(null); // To store the response data
+
+  const uploadAudio = async () => {
     const formData = new FormData();
 
     // Append the audio blob and any additional data you need
     formData.append('audio_file', audioBlob, `${timestamp}.wav`);
 
     try {
-      console.log('Sending data')
+      setUploadStatus('Sending data...');
       const response = await axios({
         method: 'post',
-        url: 'http://34.86.191.55:9000/asr?task=transcribe&encode=true&output=json&diarize=true',
+        url: 'https://34.86.191.55:9000/asr?task=transcribe&encode=true&output=json&diarize=true',
         data: formData,
         headers: {
           'Content-Type': 'multipart/form-data',
@@ -22,23 +24,33 @@ class ASRUploader extends React.Component {
         },
         timeout: 120000, // 2-minute timeout
       });
-
+      setUploadStatus('Data sent successfully!');
+      setResponseData(response.data);
       console.log('ASR Response:', response.data);
       // Handle the response as needed
     } catch (error) {
+      setUploadStatus('Error uploading audio');
       console.error('Error uploading audio:', error);
       // Handle the error as needed
     }
   };
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.uploadAudio}>Upload Audio</button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <button onClick={uploadAudio}>Upload Audio</button>
+      <p>{uploadStatus}</p>
+      {responseData && (
+        <div>
+          <p>Response:</p>
+          {/* Assuming the response data has a 'segment' object with text attributes */}
+          {responseData.segments.map((segment, index) => (
+            <p key={index}>{segment.text}</p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
 ASRUploader.propTypes = {
   audioBlob: PropTypes.instanceOf(Blob).isRequired,
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://vaco-frontend-app-bucket.storage.googleapis.com/',
   plugins: [react()],
 })
